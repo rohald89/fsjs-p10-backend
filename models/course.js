22490f7f7ba775755ecef89aa9057c8ed9b606ac
Rohald van Merode <rohald_89@live.nl>
2022-02-27 21:10:50 +0100

    added heroku config info

--- models/course.js
+++ models/course.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   class Course extends Model {
     static associate(models) {
       Course.belongsTo(models.User, {
-        as: 'user',
+        as: 'Users',
         foreignKey: {
           fieldName: 'userId',
           allowNull: false,
--- models/user.js
+++ models/user.js
@@ -4,78 +4,80 @@ const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-
     static associate(models) {
       User.hasMany(models.Course, {
-        as: "course",
+        as: 'Courses',
         foreignKey: {
-          fieldName: "userId",
+          fieldName: 'userId',
           allowNull: false,
-        }
+        },
       });
     }
   }
 
-  User.init({
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'A first name is required'
-        },
-        notEmpty: {
-          msg: 'Please provide a first name'
-        }
-      }
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'A last name is required'
-        },
-        notEmpty: {
-          msg: 'Please provide a last name'
-        }
-      }
-    },
-    emailAddress: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: {
-        msg: 'The email address you entered already exists'
-      },
-      validate: {
-        isEmail: {
-          msg: "Please enter a valid email"
-        },
-        notNull: {
-          msg: 'An email address is required'
-        }
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'A password is required',
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'A first name is required',
+          },
+          notEmpty: {
+            msg: 'Please provide a first name',
+          },
         },
       },
-      set(val){
-        if(val.length > 0){
-          const hashedPassword = bcrypt.hashSync(val, 10);
-          this.setDataValue('password', hashedPassword);
-        } else {
-          return null;
-        }
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'A last name is required',
+          },
+          notEmpty: {
+            msg: 'Please provide a last name',
+          },
+        },
+      },
+      emailAddress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          msg: 'The email address you entered already exists',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Please enter a valid email',
+          },
+          notNull: {
+            msg: 'An email address is required',
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'A password is required',
+          },
+        },
+        set(val) {
+          if (val.length > 0) {
+            const hashedPassword = bcrypt.hashSync(val, 10);
+            this.setDataValue('password', hashedPassword);
+          } else {
+            return null;
+          }
+        },
       },
     },
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User; 
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+  return User;
 };
